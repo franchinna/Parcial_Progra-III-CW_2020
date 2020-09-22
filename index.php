cbc0419554b0664686486f87aa34d38a5283806b
unknown <franqui>
2020-09-21 23:38:00 -0300

    agrego bootstrap, estilos propios y empiezo a maquetar el login

--- index.php
+++ index.php
@@ -7,49 +7,33 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="res/bootstrap/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" type="text/css" href="res/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="res/css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="res/css/style.css">
     <title>Parcial de Programacion y Clientes Web</title>
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <h1 class="navbar-brand" href="#">Navbar</h1>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse text-center" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+
 <main>
     <section id="login" class="container-fluid">
         <div class="container">
             <div class="row">
-                <div class="col col-md-4">
+                <div class="col-12">
+                    <img src="res/img/logo.png" alt="Logo de addcar" class="img-fluid">
+                </div>
+                <div class="col">
                     <h2>Identificate para poder acceder</h2>
                     <form action="">
                         <div class="form-group">
                             <label for="username">Ingrese su usuario</label>
                             <input type="text" class="form-control" id="username" placeholder="">
                         </div>
+
                         <div class="form-group">
                             <label for="password">Ingrese su password</label>
                             <input type="password" class="form-control" id="password" placeholder="">
+                        </div>
+
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-outline-primary">Identificarme</button>
                         </div>
                     </form>
                 </div>
@@ -58,7 +42,10 @@
     </section>
 </main>
 
-<script src="res/bootstrap/js/bootstrap.min.js"></script>
-<script src="res/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script type="text/javascript" src="res/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="res/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
 </body>
 </html>
