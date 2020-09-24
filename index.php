b7119cd6e93bbaca922039ad4da0bebb9a12f5be
unknown <franqui>
2020-09-24 00:11:06 -0300

    agrego iconografia, estilos. pantalla de panel-admin, y más L&F

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="res/bootstrap/css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" type="text/css" href="res/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="res/css/style.css">
+    <link rel="icon" href="res/img/ico.svg">
     <title>Parcial de Programación y Clientes Web</title>
 </head>
 <body>
@@ -16,38 +16,38 @@
     <section id="login" class="container-fluid">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h1>addcar app
-                        <img src="res/img/logo.png" alt="Logo de addcar" class="img-fluid">
-                    </h1>
+                <div class="logo col-12">
+                    <h1>addcar app</h1>
                 </div>
-                <div class="col">
-                    <h2>Identifícate para poder acceder</h2>
-                    <form action="">
+                <div class="form col">
+                    <h2>Panel Administrativo</h2>
+                    <p>Identifícate para poder acceder</p>
+                    <form action="#">
                         <div class="form-group">
                             <label for="username">Ingrese su usuario</label>
                             <input type="text" class="form-control" id="username" placeholder="">
                         </div>
-
                         <div class="form-group">
                             <label for="password">Ingrese su password</label>
                             <input type="password" class="form-control" id="password" placeholder="">
                         </div>
-
-                        <div class="text-center">
+                        <div class="button">
                             <button type="submit" class="btn btn-outline-primary">Identificarme</button>
                         </div>
                     </form>
+
+                    <!--TODO: LOGIN CON REDES SOCIALES, FB, G+, ETC-->
+
                 </div>
             </div>
         </div>
     </section>
 </main>
 
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script type="text/javascript" src="res/bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="res/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-
 </body>
 </html>
---
+++ panel-admin.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="res/css/style.css">
+    <link rel="icon" href="res/img/ico.svg">
+    <title>Parcial de Programación y Clientes Web</title>
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><h1>Addcar App</h1></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="#">Inicio <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Configuración</a>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Acerca De</a>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<main>
+    <section id="list-user" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Panel de control</h2>
+                    <p>Listado de usuarios</p>
+                </div>
+                <div class="col-12 table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col" colspan="2">Usuario</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td><a href="#"><img class="edit-user" src="res/img/edit.svg" alt="Edit User"></a></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td><a href="#"><img class="edit-user" src="res/img/edit.svg" alt="Edit User"></a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="redes col-12">
+                <ul>
+                    <li><a href="#" class="social fb"></a></li>
+                    <li><a href="#" class="social tw"></a></li>
+                    <li><a href="#" class="social ig"></a></li>
+                </ul>
+            </div>
+            <div class="bgfooter col-12"></div>
+            <div class="copy col-12">
+                <p>Todos los derechos reservados para @FrancoCinnante</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script type="text/javascript" src="res/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="res/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
